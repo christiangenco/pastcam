801177efc03f61661a6f75aa1fbd1e9cb6ca57fc
Christian Genco <christian.genco@gmail.com>
2017-03-17 19:11:32 -0500

    can load camera and camera roll objects

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
+  CameraRoll,
+  Dimensions,
+  Image,
+  ListView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
-} from 'react-native';
+} from "react-native";
+import Camera from "react-native-camera";
+var logError = require("logError");
 
 export default class ghostcam extends Component {
+  state = {
+    photos: []
+  };
+  componentDidMount() {
+    CameraRoll.getPhotos({
+      first: 5,
+      groupTypes: "SavedPhotos",
+      assetType: "Photos"
+    }).then(
+      photosRes => {
+        // console.log(JSON.stringify(photosRes));
+        this.setState({
+          photos: photosRes.edges.map(r => r.node),
+          photosPageInfo: photosRes.page_info
+        });
+        console.log(JSON.stringify(this.state.photos));
+      },
+      e => logError(e)
+    );
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Camera
+          ref={cam => this.camera = cam}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          {[...this.state.photos].map(photo => {
+            return (
+              <Image
+                key={photo.image.filename}
+                style={{ width: 10 }}
+                source={photo.image}
+              />
+            );
+          })}
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+            [CAPTURE]
+          </Text>
+        </Camera>
       </View>
     );
+  }
+  takePicture() {
+    this.camera
+      .capture()
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: "row"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    color: "#000",
+    padding: 10,
+    margin: 40
+  }
 });
 
-AppRegistry.registerComponent('ghostcam', () => ghostcam);
+AppRegistry.registerComponent("ghostcam", () => ghostcam);
