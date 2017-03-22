8caf44d30f43133dd6905a8ba863cf19a7e181e7
Christian Genco <christian.genco@gmail.com>
2017-03-22 14:26:15 -0500

    add biuld script

---
+++ build.sh
@@ -0,0 +1,4 @@
+# react-native bundle --entry-file index.ios.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
+
+react-native run-ios --configuration Release
+open ios/build/Build/Products/Release-iphonesimulator
